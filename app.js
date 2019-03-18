2655417b96a917ed203e00bf97ed3ba05a0fd533
Gabriel Bernardo Lutiano <g.lutiano@gmail.com>
2019-03-18 21:24:02 +0100

    MongoDB implementation

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-var express = require("express");
-var consolidate =  require("consolidate");
+var express = require("express"),
+    consolidate = require("consolidate"),
+    MongoClient = require("mongodb").MongoClient,
+    bodyParser = require("body-parser");
 
 var app = express();
 
-var errMsg = "";
-var name = "";
+var database;
 
 app.engine('html', consolidate.hogan);
 app.set("views", "static");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
-    name = req.query.username;
-    var password = req.query.password;
-    var description = req.query.description;
-    var address = req.query.address;
-
-    if(password != undefined && password != "123pass"){
-        errMsg = "The password is incorrect";
-        res.redirect("/authentication");
-    }
-
-    res.render("main.html", {username: name, description: description, address: address});
+MongoClient.connect("mongodb://localhost:27018/", { useNewUrlParser: true }, (err, db) => {
+    dbo = db.db("incidents_db");
+    if(err) throw err;
+    database = dbo;
 });
 
-app.get("/incident", function(req, res){
-    name = req.query.username;
-    if(name != undefined){
-        res.render("incident.html", {username: name});
-    }else{
-        res.redirect("/authentication");
-    }
+app.get("/", function (req, res) {
+    database.collection("incidents").find().toArray((err, doc) => {
+        if (err) throw err;
+        var list = {};
+        list.incidents = doc;
+        res.render("main.html", list);
+    }); 
 });
 
-app.get("/authentication", function(req, res){
-    res.render("authentication.html", {errMsg: errMsg});
+app.get("/incident", function (req, res) {
+    res.render("incident.html");
 });
 
+app.get("/authentication", function (req, res) {
+    res.render("authentication.html");
+});
+
+app.post("/incident", function(req, res){
+    var address = req.body.address;
+    var description = req.body.description;
+    var new_incident = {"description": description, "address": address, "author": "", "date": 18/03/2019};
+    database.collection("incidents").insertOne(new_incident, function(err, doc){
+        if(err) throw err;
+        res.redirect("/");       
+    });
+});
 
 app.use(express.static('main'));
 app.listen(8080);
+
